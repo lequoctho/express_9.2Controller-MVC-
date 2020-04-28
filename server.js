8384166230fc0c815f511d79f056867405bfe8f7
Glitch (clear-amenable-path) <none>
2020-04-28 14:18:29 +0000

    🚕😬 Checkpoint ./views/users/update.pug:15532837/157 ./views/users/index.pug:15532837/20 ./server.js:15532837/3116 ./routes/user.route.js:15532837/1116

--- server.js
+++ server.js
@@ -58,49 +58,6 @@ app.post("/books/create", (req, res) => {
   res.redirect("/books");
 });
 
-// user
-const adapterUser = new FileSync('dbUser.json');
-const dbUser = low(adapterUser);
-
-// Set some defaults
-dbUser.defaults({users: []}).write();
-
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/users", (req, res) => {
-  res.render('indexUsers',{
-    users: dbUser.get('users').value()
-  });
-});
-
-app.get("/users/:id/delete", (req, res)=> {
-  var id = req.params.id;
-  var book = dbUser.get('users').find({id: id}).value();
-  dbUser.get('users').remove(book).write();
-  res.redirect("/users");
-});
-
-app.get("/users/:id/update", (req, res) => {
-  var id = req.params.id;
-  var user = dbUser.get('users').find({id: id}).value();
-  res.render('updateUser',{
-    user: user
-  });
-});
-
-app.post("/users/:id/update", (req, res)=> {
-  var id = req.params.id;
-  var text = req.body;
-
-  dbUser.get('users').find({id: id}).assign(text).write();
-  
-  res.redirect("/users");
-});
-
-app.post("/users/create", (req, res) => {
-  req.body.id = shortid.generate();
-  dbUser.get('users').push(req.body).write();
-  res.redirect("/users");
-});
 
 // listen for requests :)
 app.listen(process.env.PORT, () => {
