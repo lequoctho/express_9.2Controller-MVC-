be2e5703f0c9e4bb5ed70efaee0d2d1484e6485f
Glitch (clear-amenable-path) <none>
2020-04-29 16:44:51 +0000

    💺🛣 Checkpoint ./routes/book.route.js:15532837/1551 ./controllers/book.controller.js:15532837/1564

--- routes/book.route.js
+++ routes/book.route.js
@@ -1,38 +1,16 @@
 const express = require("express");
-const shortid = require("shortid");
 
-const db = require("../db");
+const controller = require("../controllers/book.controller");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render('books/index',{
-    books: db.get('books').value()
-  });
-});
+router.get("/", controller.index);
 
-router.get("/:id/delete", (req, res)=> {
-  var id = req.params.id;
-  var book = db.get('books').find({id: id}).value();
-  db.get('books').remove(book).write();
-  res.redirect("/books");
-});
+router.get("/:id/delete", controller.delete);
 
-router.get("/update/:id", (req, res) => {
-  var id = req.params.id;
-  var book = db.get('books').find({id: id}).value();
-  res.render('books/update',{
-    book: book
-  });
-});
+router.get("/update/:id", controller.update);
 
-router.post("/:id/update", (req, res)=> {
-  var id = req.params.id;
-  var text = req.body;
-  console.log(text);
-  db.get('books').find({id: id}).assign(text).write();
-
-  res.redirect("/books");
-});
+router.post("/:id/update", controller.updatePost);
 
 router.post("/create", (req, res) => {
   req.body.id = shortid.generate();
