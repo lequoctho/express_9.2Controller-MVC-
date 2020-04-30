02c82d65755320129a0aa0473e01b3d371a26b78
Glitch (clear-amenable-path) <none>
2020-04-30 10:30:42 +0000

    🏋🔨 Checkpoint ./db.json:15532837/1225 ./controllers/transaction.controller.js:15532837/132 ./views/transaction/index.pug:15532837/76

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -4,7 +4,8 @@ const shortid = require("shortid");
 module.exports.index = (req, res) => {
   var users = db.get('users').value();
   var books = db.get('books').value();
-  var transactions = db.get('transactions').value().map((objTransaction) => {
+  var transactionsDB = db.get('transactions').value();
+  var transactions = transactionsDB.map((objTransaction) => {
     console.log(objTransaction);
     var user = users.find(user=>user.id === objTransaction.userId);
     var book = books.find(book=>book.id === objTransaction.bookId);
@@ -22,6 +23,7 @@ module.exports.index = (req, res) => {
   
 module.exports.create = (req, res) => {
   req.body.id = shortid.generate();
+  req.body.complete = "false";
   db.get('transactions').push(req.body).write();
 
   res.redirect("/transactions");
